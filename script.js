2d415d219063cae6e77c38540a278dd53d11f9f5
alexisonfuego <29289359+Alexisonfuego@users.noreply.github.com>
2024-10-19 20:51:33 +0900

    Added function to receive inputs and add to display

--- script.js
+++ script.js
@@ -19,9 +19,79 @@ let firstNumber
 let operator
 let secondNumber
 let result
+let operationStarted = false
+let operatorSelected = false
+
 
 let operate = function (operator, x, y) {
     return (operator(x, y))
 }
 
-console.log(operate(multiply, 3, 2));
+// console.log(operate(multiply, 3, 2));
+
+let firstDisplayNumber = document.querySelector(".firstNumber")
+let secondDisplayNumber = document.querySelector(".secondNumber")
+let resultDisplayNumber = document.querySelector(".result")
+let inputButton = document.querySelectorAll(".input")
+
+
+inputButton.forEach((input) => {
+    input.addEventListener('click', (event) => {
+        //take input value
+        const inputText = event.target.textContent
+
+        //generate first number
+        if (input.classList.contains('number') && !operatorSelected) {
+            if (firstNumber === undefined) {
+                firstNumber = inputText
+                firstDisplayNumber.textContent = firstNumber
+            } else {
+                firstNumber = (firstNumber + inputText)
+                firstDisplayNumber.textContent = firstNumber
+            }
+            operationStarted = true
+        
+        //select operator
+        } else if (input.classList.contains('operator') && operationStarted) {
+            if (secondNumber === undefined) {
+                operator = inputText
+                secondDisplayNumber.textContent = (operator + " ")
+                operatorSelected = true
+            }
+
+        //generate second number
+        } else if (input.classList.contains('number') && operationStarted) {
+            if (secondNumber === undefined) {
+                secondNumber = inputText
+                secondDisplayNumber.textContent = (operator + " " + secondNumber)
+            } else {
+                secondNumber = (secondNumber + inputText)
+                secondDisplayNumber.textContent = (operator + " " + secondNumber)
+            }
+            operationStarted = true
+        }
+    })
+})
+
+
+
+
+
+
+
+
+ //     if (firstNumber === undefined) {
+    //         firstNumber = inputText
+    //         firstDisplayNumber.textContent = firstNumber
+    //     } else if (operatorSelected) {
+    //         secondNumber = event.target.textContent
+    //         secondDisplayNumber.textContent = (operator + " " + secondNumber)
+    //     } else if (input.classList.contains('operator')) {
+    //         operator = event.target.textContent
+    //         operatorSelected = true
+    //         secondDisplayNumber.textContent = (operator + " ")
+    //     }
+    //     else {
+    //         firstNumber = (firstNumber + event.target.textContent)
+    //         firstDisplayNumber.textContent = firstNumber
+    //         }
