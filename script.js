7e92e4e83443fdd2d593611c70e9af6dff0c15ee
alexisonfuego <29289359+Alexisonfuego@users.noreply.github.com>
2024-10-20 22:25:02 +0900

    removed unnecessary breaks in operate func

--- script.js
+++ script.js
@@ -15,19 +15,15 @@ let operate = function (operator, x, y) {
   switch (operator) {
     case "+":
       return add(x, y);
-      break;
 
     case "-":
       return subtract(x, y);
-      break;
 
     case "*":
       return multiply(x, y);
-      break;
 
     case "/":
       return divide(x, y);
-      break;
 
     default:
       return "ERROR";
